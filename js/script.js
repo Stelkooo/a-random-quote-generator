fd18e6cc3636532645dd48c407b34f17b4a487fd
Stelko <stilyanmtoskov@gmail.com>
2022-06-17 21:53:35 +0100

    quotes arr created

--- js/script.js
+++ js/script.js
@@ -10,8 +10,34 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-
-
+let quotes = [
+  {
+    quote: "Scissors cuts paper, paper covers rock, rock crushes lizard, lizard poisons Spock, Spock smashes scissors, scissors decapitates lizard, lizard eats paper, paper disproves Spock, Spock vaporizes rock, and as it always has, rock crushes scissors.",
+    source: "Sheldon",
+    citation: "The Big Bang Theory",
+    year: 2012
+  },
+  {
+    quote: "I am the king of the nerds.",
+    source: "Leonard",
+    citation: "The Big Bang Theory"
+  },
+  {
+    quote: "As my mom used to say, ‘When you’re doing a puzzle, it’s like you’ve got a thousand friends.’ She was full of fun lies like that.",
+    source: "Amy",
+    citation: "The Big Bang Theory"
+  },
+  {
+    quote: "What up, Shel-Bot?",
+    source: "Penny",
+    citation: "The Big Bang Theory"
+  },
+  {
+    quote: "I haven’t cried this hard since Toy Story 3.",
+    source: "Raj",
+    citation: "The Big Bang Theory"
+  }
+];
 
 /***
  * `getRandomQuote` function
